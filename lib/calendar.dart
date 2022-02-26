e4c3a8c41e83b938014acf5c11a0a414a860d455
HoleInWan <y.kurohata.h5@gmail.com>
2022-02-26 11:07:41 +0900

    introduction calender

---
+++ lib/calendar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("calender class"),
+          centerTitle: true,
+        ),
+        body: TableCalendar(
+          focusedDay: _focusedDay,
+          firstDay: DateTime(2020),
+          lastDay: DateTime(2030),
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay; // update `_focusedDay` here as well
+            });
+          },
+          startingDayOfWeek: StartingDayOfWeek.monday,
+          calendarFormat: _calendarFormat,
+          onFormatChanged: (format) {
+            if (_calendarFormat != format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            }
+          },
+          onPageChanged: (focusedDay) {
+            _focusedDay = focusedDay;
+          },
+        ));
+  }
+}
